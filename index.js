02e35a3a4419ae61512ea09d673690693f02b99b
Landon <landoncramer1@gmail.com>
2023-09-13 20:21:13 -0700

    array lab 2

--- index.js
+++ index.js
@@ -1,33 +1,72 @@
-// Write your solution here!
+// // Write your solution here!
+// let cats = ["Milo", "Otis", "Garfield"];
+// function destructivelyAppendCat(name) {
+//     cats.push(name);
+// }
+// function destructivelyPrependCat(name) {
+//     cats.unshift(name);
+// }
+// function destructivelyRemoveLastCat() {
+//     cats.pop();
+// }
+// function destructivelyRemoveFirstCat() {
+//     cats.shift();
+// }
+// function appendCat(name) {
+//     let newArr = [...cats, name];
+//     return newArr;
+// }
+// function prependCat(name) {
+//     let newArr = cats.slice();
+//     newArr.unshift(name);
+//     return newArr;
+// }
+// function removeLastCat() {
+//     let newArr = [...cats];
+//     newArr.pop();
+//     return newArr;
+// }
+// function removeFirstCat() {
+//     let newArr = cats.slice();
+//     newArr.shift();
+//     return newArr;
+// }
+
 let cats = ["Milo", "Otis", "Garfield"];
+
 function destructivelyAppendCat(name) {
-    cats.push(name);
+    return cats.push(name);
 }
+
 function destructivelyPrependCat(name) {
-    cats.unshift(name);
+    return cats.unshift(name);
 }
-function destructivelyRemoveLastCat() {
-    cats.pop();
-}
+
 function destructivelyRemoveFirstCat() {
-    cats.shift();
+    return cats.shift();
+}
+
+function destructivelyRemoveLastCat() {
+    return cats.pop();
 }
 function appendCat(name) {
-    let newArr = [...cats, name];
-    return newArr;
+    let newArr = cats.slice();
+     newArr.push(name);
+     return newArr;
 }
 function prependCat(name) {
-    let newArr = cats.slice();
-    newArr.unshift(name);
+    let newArr = [name, ...cats];
     return newArr;
 }
+
 function removeLastCat() {
     let newArr = [...cats];
     newArr.pop();
     return newArr;
 }
+
 function removeFirstCat() {
-    let newArr = cats.slice();
+    let newArr = [...cats];
     newArr.shift();
     return newArr;
 }
